936b522ae598c89308875bc047bbea1b070e1d36
Samir Jaber <samir.jaber@neki-it.com.br>
2025-07-29 11:37:49 -0300

    cria estrutura inicial do projeto

---
+++ src/main/java/ufjf_dcc025/franquiasystem/FranquiaSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ufjf_dcc025.franquiasystem;
+
+/**
+ *
+ * @author Administrador
+ */
+public class FranquiaSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
